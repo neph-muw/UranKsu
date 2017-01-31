dc46b2544af21c31f235b2808379775e2b28418d
Roman Mykitchak <deviphone@i.ua>
2017-01-31 12:52:07 +0200

    First commit

---
+++ UranKsu/UranKsu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UranKsu
+//
+//  Created by Roman Mykitchak on 1/31/17.
+//  Copyright © 2017 Roman Mykitchak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UranKsu/UranKsu/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UranKsu
+//
+//  Created by Roman Mykitchak on 1/31/17.
+//  Copyright © 2017 Roman Mykitchak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
